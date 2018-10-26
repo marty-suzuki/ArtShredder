9427727a46b9d3b7e1bba589029ff2100d13ccab
marty-suzuki <s1180183@gmail.com>
2018-10-27 01:33:56 +0900

    add Common

--- ArtShredder/AppDelegate.swift
+++ ArtShredder/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         GADMobileAds.configure(withApplicationID: AdMobConfig.make().appID)
+        
         self.window = {
             let window = UIWindow(frame: UIScreen.main.bounds)
             window.rootViewController = ShredderViewController()
